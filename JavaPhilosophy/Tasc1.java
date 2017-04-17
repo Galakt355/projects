37dc9f0dd4aa35d88fdc2cfa817badea3b4ba79a
Galakt355 <mga3556276@gmail.com>
2017-04-17 10:40:37 +0400

    ghost

---
+++ JavaPhilosophy/Tasc1.java
@@ -0,0 +1,40 @@
+package JavaRush.Level_1.Lecture4;
+
+/**
+ * Created by mikishevga on 10.04.17.
+ */
+public class Tasc1
+{
+    public static void main(String[] args) {
+		System.out.print
+        System.out.println("Я стою на пороге Вселенной,");
+        System.out.println("Вихри космоса веют в лицо,");
+        System.out.println("Все, что вечно, и все, что мгновенно,");
+        System.out.println("Припорошено звездной пыльцой.");
+
+        System.out.println("К звездам я прикасаюсь,");
+        System.out.println("Зеркальностью мира пленен,");
+        System.out.println("Я могу неотступно годами");
+        System.out.println("Ждать привета из бездны времен.");
+
+        System.out.println("Верю: в будущем, с космосом слиты,");
+        System.out.println("Всю Галактику сможем пройти");
+        System.out.println("Кто-то встретит свою Аэлиту,");
+        System.out.println("Кто-то — звездных субстанций дожди.");
+
+        System.out.println("А пока корабли на орбите");
+        System.out.println("За витком совершают виток,");
+        System.out.println("Я сплетаю фантастики нити");
+        System.out.println("В многоцветный рассказов венок.");
+
+        System.out.println("");
+        System.out.println("В тумане моря голубом!..");
+        System.out.println("Что ищет он в стране далекой?");
+        System.out.print("Что кинул он в краю родном?...");
+    }
+
+    public static void sum(int a, int b) {
+        int c = a + b;
+        System.out.print(c);
+    }
+}
