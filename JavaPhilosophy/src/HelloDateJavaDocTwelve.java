ff176646cdfd1ccb771ea6a367a6a8940452c636
Galakt355 <mga3556276@gmail.com>
2017-04-19 15:53:32 +0400

    Задачи

--- JavaPhilosophy/src/HelloDateJavaDocTwelve.java
+++ JavaPhilosophy/src/HelloDateJavaDocTwelve.java
@@ -1,12 +1,20 @@
+//: object/HelloDateJavaDocTwelve.java
 import java.util.*;
 
-public class HelloDateJavaDocTwelve{
-	public static void main(String[] args){		
-		System.out.print("Привет, сегодня: ");
-		System.out.print(new Date());
-	}
-	
-	public static void matod(){
-		System.out.print("Привет мир !");
-	}
-}
+/**Первая программа - пример книги
+ * Выводит строку и текущее число.
+ * @author Геннадий Микишев
+ * @version 1.0
+ */
+class HelloDateJavaDocTwelve
+{
+    /**
+     * Точка входа в класс и приложение
+     * @param args Массив строковых аргументов
+     * @throws exceptions Исключения не выдаются
+     */
+    public static void main(String[] args){
+        System.out.print("Привет, сегодня: ");
+        System.out.print(new Date());
+    }
+}/* Output: (55% match) Дата и время текущего дня *///:~
